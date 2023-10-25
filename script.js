e445faece77939d38bee31cd08fce2e8b3d1b887
joseph farrell <224jfarrell@frhsd.com>
2023-10-25 10:42:48 -0400

    almost there mayb??e

--- script.js
+++ script.js
@@ -26,41 +26,55 @@ let A = document.getElementById('green');
 let B = document.getElementById('red');
 let C = document.getElementById('blue');
 let D = document.getElementById('yellow');
+let E = document.getElementById('greenbtn');
+let F = document.getElementById('redbtn');
+let G = document.getElementById('bluebtn');
+let H = document.getElementById('yellowbtn');
+
+
 function reset(color){
     if(color == 'green'){
         A.style.backgroundColor = '#3f3';
         A.style.boxShadow = '0 0 0 0';
+        A.style.borderStyle = 'outset';
     } else if(color == 'red'){
         B.style.backgroundColor = "#f33";
         B.style.boxShadow = '0 0 0 0';
+        B.style.borderStyle = 'outset';
     } else if(color == 'blue'){
         C.style.backgroundColor = "#33f";
         C.style.boxShadow = "0 0 0 0";
+        C.style.borderStyle = 'outset';
     } else if(color == 'yellow'){
         D.style.backgroundColor = "#ff3";
         D.style.boxShadow = '0 0 0 0';
+        D.style.borderStyle = 'outset';
     }
 }
 
 function playNoise(color){
     if(color == 'green'){
         A.style.backgroundColor = '#8f8';
-        A.style.boxShadow = '0 0 20px #8f8';
+        A.style.boxShadow = '0 0 20px 10px #8f8';
+        A.style.borderStyle = 'inset';
         playFrequency(440);
         setTimeout(function(){ reset('green') }, 400);
     } else if(color == 'red'){
         B.style.backgroundColor = '#f88';
-        B.style.boxShadow = '0 0 20px #f88';
+        B.style.boxShadow = '0 0 20px 10px #f88';
+        B.style.borderStyle = 'inset';
         playFrequency(554.3653);
         setTimeout(function(){ reset('red') }, 400);
     } else if(color == 'blue'){
         C.style.backgroundColor = '#88f';
-        C.style.boxShadow = '0 0 20px #88f';
+        C.style.boxShadow = '0 0 20px 10px #88f';
+        C.style.borderStyle = 'inset';
         playFrequency(659.2551);
         setTimeout(function(){ reset('blue') }, 400);
     } else if(color == 'yellow'){
         D.style.backgroundColor = '#ff8';
-        D.style.boxShadow = '0 0 20px #ff8';
+        D.style.boxShadow = '0 0 20px 10px #ff8';
+        D.style.borderStyle = 'inset';
         playFrequency(880);
         setTimeout(function(){ reset('yellow') }, 400);
     }
@@ -105,10 +119,6 @@ if(score == 1){
 
 
 let actualInput;
-let E = document.getElementById('greenbtn');
-let F = document.getElementById('redbtn');
-let G = document.getElementById('bluebtn');
-let H = document.getElementById('yellowbtn');
 
 function disable(){
     E.disabled = true;
@@ -142,11 +152,48 @@ function check(input){
     if(actualInput == expectedInput){
         score += 1;
         scoreText.innerText = `Score: ${score}`;
+        roundList.push(expectedInput);
         nextRound();
     } else {
         scoreText.innerText = "you lose"
         disable();
         document.getElementById("start").innerText = "Refresh";
+        switch(expectedInput){
+            case 'green':
+                A.style.backgroundColor = '#8f8';
+                A.style.boxShadow = '0 0 20px 10px #8f8';
+                break;
+            case 'red':
+                B.style.backgroundColor = '#f88';
+                B.style.boxShadow = '0 0 20px 10px #f88';
+                break;
+            case 'blue':
+                A.style.backgroundColor = '#88f';
+                A.style.boxShadow = '0 0 20px 10px #88f';
+                break;
+            case 'yellow':
+                D.style.backgroundColor = '#ff8';
+                D.style.boxShadow = '0 0 20px 10px #ff8';
+                break;
+        }
+        switch(actualInput){
+            case 'green':
+                A.style.backgroundColor = '#000';
+                A.style.borderColor = '#f00';
+                break;
+            case 'red':
+                B.style.backgroundColor = '#000';
+                B.style.borderColor = '#f00';
+                break;
+            case 'blue':
+                C.style.backgroundColor = '#000';
+                C.style.borderColor = '#f00';
+                break;
+            case 'yellow':
+                D.style.backgroundColor = '#000';
+                D.style.borderColor = '#f00';
+                break;
+        }
     }
 }
 
@@ -156,8 +203,7 @@ function refresh(){
     }
 }
 
-let roundList = [0];
-let timePassed = 0;
+let roundList = [];
 
 function stop(){
     clearInterval(repeat);
@@ -167,23 +213,39 @@ let repeat;
 function nextRound() {
     disable();
     stop();
-    repeat = setInterval(randomColor, 500);
-    roundList.push(expectedInput);
-    let delay = 500 * roundList.length;
+    repeat = setInterval(thisCouldBreakEverything, 500);
+    // repeat = setInterval(randomColor, 500);
+    let delay = 500 * (roundList.length);
     console.log(delay);
     setTimeout(stop, delay);
-    setTimeout(enable, delay + 500);
+    setTimeout(randomColor, delay + 500);
+    setTimeout(enable, delay + 1000);
     console.log(roundList);
 }
 
 function firstRound(){
     randomColor();
     console.log(expectedInput);
-    roundList[0] = expectedInput;
     console.log(roundList);
 }
 
-
-/*  todo 
-    - make lightup require click on correct square
-*/
+function thisCouldBreakEverything(){
+    for(i = 0; i < roundList.length; setInterval(i++, 500)){
+        console.log(i);
+        console.log(roundList[i]);
+        switch(roundList[i]){
+            case 'green':
+                playNoise('green');
+                break;
+            case 'red':
+                playNoise('red');
+                break;
+            case 'blue':
+                playNoise('blue');
+                break;
+            case 'yellow':
+                playNoise('yellow');
+                break;
+        }
+    }
+}
